ac6113920cdacfec806dfe022989ca18a2bd2db8
Badru <badruakfm@gmail.com>
2023-12-10 01:43:58 +0700

    fix url api

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export const API = axios.create({
     // baseURL: 'http://localhost:4000/v1/'
-    baseURL: 'https://simple-blog-rose.vercel.app/'
+    baseURL: 'https://simple-blog-rose.vercel.app/v1/'
 })
 
 export const getConfig = async () => {
